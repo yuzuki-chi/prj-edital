66c73326b165d41b27d92a72c9d47024fb3e9b70
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-06-08 09:56:22 +0900

    develop contentの刷新

---
+++ edital/student/paper.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+</head>
+<body>
+    <h1>5. 比を簡単にする（１）</h1>
+    <h2>次の（１）〜（２）の比を簡単にしなさい。</h2>
+    <div>
+        <div>(1) 24:42</div>
+        <div>式</div>
+        <div>答え</div>
+    </div>
+    <button onclick="submitPaper()">提出する</button>
+    <script>
+        function submitPaper() {
+            alert('提出しました');
+        }
+    </script>
+</body>
+</html>
---
+++ edital/teacher/question_list.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>問題一覧</title>
+</head>
+<body>
+    <h1>問題一覧</h1>
+    <ul>
+        <li>かけ算のきまり</li>
+        <li>円と球</li>
+        <li>あまりのあるわり算</li>
+        <li>少数の引き算</li>
+        <li>三角形と四角形</li>
+        <li>2けたのかけ算</li>
+        <li>かけ算の筆算</li>
+    </ul>
+</body>
+</html>
